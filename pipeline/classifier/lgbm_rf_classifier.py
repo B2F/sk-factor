95ab4cbf3025d7ec1ada5b07c40c5d326836d3af
B2F <d4rb2f@gmail.com>
2024-07-30 20:51:28 +0200

    Add default param for random forest

--- pipeline/classifier/lgbm_rf_classifier.py
+++ pipeline/classifier/lgbm_rf_classifier.py
@@ -5,6 +5,20 @@ class LgbmRfClassifier(BaseLgbm):
 
     def getEstimator(self):
 
-        self._params['boosting_type'] = 'rf'
-        classifier = lgb.LGBMClassifier(**self._params)
+        classifier = lgb.LGBMClassifier(
+            boosting_type="rf",
+            num_leaves=self._params['num_leaves'],
+            colsample_bytree=.5,
+            n_estimators=400,
+            min_child_weight=5,
+            min_child_samples=10,
+            subsample=.632,
+            subsample_freq=1,
+            min_split_gain=0,
+            reg_alpha=0,
+            reg_lambda=5, # L2 regularization
+            n_jobs=3,
+            seed=self._params['seed']
+        )
+
         return ('lgbm_rf_classifier', classifier)
