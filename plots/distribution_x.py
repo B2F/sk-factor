8a22f7eb674b05b325bd7ee553752b106b79ef3a
B2F <d4rb2f@gmail.com>
2024-08-08 11:53:34 +0200

    Add distribution x, y plots

--- plots/distribution_x.py
+++ plots/distribution_x.py
@@ -1,7 +1,7 @@
 from plots.base_report import Report
 import matplotlib.pyplot as plt
 
-class Distribution(Report):
+class DistributionX(Report):
 
     def plot(self):
 
@@ -9,10 +9,10 @@ class Distribution(Report):
       if self._config['eda'].get('n_bins'):
           n_bins = eval(self._config['eda']['n_bins'])
 
-      if self._config['eda'].get('distribution'):
-          feature = eval(self._config['eda']['distribution'])
+      if self._config['eda'].get('distribution_x'):
+          feature = self._config['eda']['distribution_x']
           plt.hist(self._x[feature], bins=n_bins)
       else:
           plt.hist(self._x, bins=n_bins)
 
-      return 'distribution'
+      return 'distribution_x'
---
+++ plots/distribution_y.py
@@ -0,0 +1,14 @@
+from plots.base_report import Report
+import matplotlib.pyplot as plt
+
+class DistributionY(Report):
+
+    def plot(self):
+
+      n_bins = 5
+      if self._config['eda'].get('n_bins'):
+          n_bins = eval(self._config['eda']['n_bins'])
+
+      plt.hist(self._y, bins=n_bins)
+
+      return 'distribution_y'
