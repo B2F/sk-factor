c0155a11e62d99f934116e89a066c4491862f537
B2F <d4rb2f@gmail.com>
2024-08-08 11:52:26 +0200

    Adds eda figsize, dpi, and multi plot refresh

--- plots/base_report.py
+++ plots/base_report.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from abc import ABC, abstractmethod
 import matplotlib.pyplot as plt
+import seaborn as sns
 import time
 import os
+from matplotlib import rcParams
 
 class Report(ABC):
 
@@ -28,6 +30,9 @@ class Report(ABC):
         self._imagesDirectory = config['eda']['images_directory']
         self._imagesExtension = config['eda']['images_extension']
         self.__identifier = identifier
+        # figure size in inches
+        if type(eval(config['eda'].get('figsize'))) is tuple:
+            rcParams['figure.figsize'] = config['eda']['figsize']
 
     def getImageFilepath(self, filename):
         directory = f'{self._imagesDirectory}/{self.__identifier}'
@@ -44,9 +49,13 @@ class Report(ABC):
 
     def run(self):
         plotId = self.plot()
+        dpi = 100
+        if self._config['eda'].get('dpi'):
+            dpi=self._config['eda']['dpi']
         if self._showPlot:
             plt.tight_layout()
             plt.show()
         if self._saveImage:
-            plt.savefig(self.getImageFilepath(plotId))
+            plt.savefig(self.getImageFilepath(plotId), dpi=dpi)
             print(os. getcwd()  + '/' + self.getImageFilepath(plotId) + " written to disk")
+        plt.clf()
