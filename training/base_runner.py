8cb7d37fac0cf00524b6dd873763246aad83d432
B2F <d4rb2f@gmail.com>
2024-08-09 14:16:41 +0200

    Adds base_runner constructor

--- training/base_runner.py
+++ training/base_runner.py
@@ -1,6 +1,20 @@
 from abc import abstractmethod
+import pandas as pd
 
 class BaseRunner():
+
+    _config: dict
+    _x: pd.DataFrame
+    _y: pd.DataFrame
+    _labels: pd.DataFrame
+    __identifier: str
+
+    def __init__(self, config, x, y, labels, identifier = None):
+        self._config = config
+        self._x = x
+        self._y = y
+        self._labels = labels
+        self.__identifier = identifier
 
     @abstractmethod
     def run(self, pipeline, cv):
