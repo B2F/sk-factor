1ed7f4aa1856b50ca9b359613207de9599e83112
B2F <d4rb2f@gmail.com>
2024-08-14 16:04:12 +0200

    Ensure a plugin's package has a valid class

--- src/engine/plugins.py
+++ src/engine/plugins.py
@@ -1,6 +1,15 @@
 import importlib
+import re
+from plugins.pipeline.base_estimator import BaseEstimator
+from plugins.plots.base_report import Report
+from plugins.preprocess.base_preprocessor import BasePreprocessor
+from plugins.preprocess.base_transformer import BaseTransformer
+from plugins.split.base_cv import BaseCv
+from plugins.training.base_runner import BaseRunner
 
 class Plugins():
+    """ Plugins factory.
+    """
 
     @staticmethod
     def create(package: str, module: str, *args):
@@ -12,8 +21,42 @@ class Plugins():
         else:
             moduleName = module
             classModule = importlib.import_module(f"plugins.{package}.{moduleName}")
+
         classTokens = moduleName.split('_')
         className = ''.join(ele.title() for ele in classTokens)
         className = getattr(classModule, className)
 
-        return className(*args)
+        object = className(*args)
+
+        Plugins.checkPackageClass(object, package)
+
+        return object
+
+    @staticmethod
+    def checkPackageClass(object, package):
+
+        # Check validity relating to package.
+        packagesClasses = {
+            'pipeline': BaseEstimator,
+            'plots': Report,
+            'preprocess.preprocessor': BasePreprocessor,
+            'preprocess.transformer': BaseTransformer,
+            'split': BaseCv,
+            'training': BaseRunner,
+            # 'predictions': BasePredictions,
+        }
+
+        matchingPackage = ''
+        for regularPackage in packagesClasses:
+            if not re.match(regularPackage, package):
+                continue
+            else:
+                matchingPackage = regularPackage
+                break
+
+        if not matchingPackage:
+            raise Exception(f'Plugin''s package {package} does not exist.')
+
+        expectedClass = packagesClasses[matchingPackage]
+        if not isinstance(object, expectedClass):
+            raise Exception(f'Plugin of package {package} must be of class {expectedClass}.')
