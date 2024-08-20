1cc3502803c388044c208170e9e03f913c9b190b
B2F <d4rb2f@gmail.com>
2024-08-20 11:38:37 +0200

    Remove random_state from split without shuffle

--- plugins/split/kfold.py
+++ plugins/split/kfold.py
@@ -4,5 +4,5 @@ from plugins.split.base_cv import BaseCv
 class Kfold(BaseCv):
 
     def split(self):
-        k = KFold(n_splits = self._nSplits, random_state = self._random_state)
+        k = KFold(n_splits = self._nSplits)
         return k.split(self._x, self._y)
--- plugins/split/kfold_stratified_group.py
+++ plugins/split/kfold_stratified_group.py
@@ -4,5 +4,5 @@ from plugins.split.base_cv import BaseCv
 class KfoldStratifiedGroup(BaseCv):
 
     def split(self):
-        k = StratifiedGroupKFold(n_splits = self._nSplits, random_state=self._random_state)
+        k = StratifiedGroupKFold(n_splits = self._nSplits)
         return k.split(self._x, self._y, self._groups)
