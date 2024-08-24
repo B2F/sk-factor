1878b81fbe82f0f82966a35650fde79cf76c1c09
B2F <d4rb2f@gmail.com>
2024-08-24 15:50:54 +0200

    Adds binary predictor with credit_card_fraud.toml example

--- plugins/predictions/binary.py
+++ plugins/predictions/binary.py
@@ -4,14 +4,29 @@
 """
 
 from plugins.predictions.base_predictor import BasePredictor
+from src.engine.model import Model
+import pandas as pd
 
 class Binary(BasePredictor):
 
-    def _predict(self, model):
+    def _predict(self, model: Model):
 
-        probas = model.predict_proba(self._x)
+        probas = model.pipeline.predict_proba(self._x)[:, 1]
 
-        # Adjust predictions based on the threshold
-        adjusted_predictions = (probas[:, 1] >= self._threshold).astype(int)
+        # Output valid classes given the configured predictions threshold:
+        estimations = probas > self._config.get('predictions', 'threshold')
 
-        return adjusted_predictions
+        testRange = range(len(estimations))
+        probasDf = pd.DataFrame(data = probas, columns = ['proba'])
+        threshold = pd.DataFrame(data = [self._threshold for _ in testRange], columns=['threshold'])
+        estimations = pd.DataFrame(data = estimations, columns=['estimation'])
+
+        outputColumns = [probasDf, threshold, estimations]
+        if self._config.eq('predictions', 'keep_data', True):
+            outputColumns = [self._x] + outputColumns
+
+        predictions_output = pd.concat(outputColumns, sort=False, axis="columns")
+
+        print(predictions_output)
+
+        return predictions_output
